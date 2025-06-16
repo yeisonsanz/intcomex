db4deccf17ea8f5f10289379cb0bf1f22e4dc595
yeisonsanz <yeisonkb@gmail.com>
2025-06-16 07:35:38 -0500

    proyecto base

---
+++ src/main/java/com/intcomex/ApiRestIntcomexApplication.java
@@ -0,0 +1,13 @@
+package com.intcomex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestIntcomexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestIntcomexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/intcomex/ApiRestIntcomexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intcomex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestIntcomexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
