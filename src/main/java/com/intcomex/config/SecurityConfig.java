e0f5a1db972e74af804976816e9132a077fb74b1
yeisonsanz <yeisonkb@gmail.com>
2025-06-16 12:45:37 -0500

    feat: implementacion de spring security

--- src/main/java/com/intcomex/config/SecurityConfig.java
+++ src/main/java/com/intcomex/config/SecurityConfig.java
@@ -1,4 +1,35 @@
 package com.intcomex.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/products/generate").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password("{noop}admin123")
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
 }
